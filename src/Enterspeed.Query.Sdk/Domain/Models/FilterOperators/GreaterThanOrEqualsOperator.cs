6b67c4af95c42e2f9db769eb086271bedb281f70
BjarkeKarlsen <bka@enterspeed.com>
2026-01-23 14:00:23 +0100

    Update name of type

--- src/Enterspeed.Query.Sdk/Domain/Models/FilterOperators/GreaterThanOrEqualsOperator.cs
+++ src/Enterspeed.Query.Sdk/Domain/Models/FilterOperators/GreaterThanOrEqualsOperator.cs
@@ -1,6 +1,6 @@
 namespace Enterspeed.Query.Sdk.Domain.Models.FilterOperators
 {
-    public class GreaterThanOrEquals<TValue> : FilterOperator<TValue>
+    public class GreaterThanOrEqualsOperator<TValue> : FilterOperator<TValue>
     {
         public override string Operator => "greaterThanOrEquals";
         public override TValue Value { get; set; }
