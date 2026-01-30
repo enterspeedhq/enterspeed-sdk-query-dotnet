6da92d3ed021569b225e20ae1332624165adbd51
BjarkeKarlsen <bka@enterspeed.com>
2026-01-30 15:44:11 +0100

    QueryError looks like the Error from Query SDK

--- src/Enterspeed.Query.Sdk/Api/Models/Response/QueryError.cs
+++ src/Enterspeed.Query.Sdk/Api/Models/Response/QueryError.cs
@@ -3,32 +3,36 @@ namespace Enterspeed.Query.Sdk.Api.Models.Response
     /// <summary>
     /// Represents an error that occurred during query execution.
     /// </summary>
-    public class QueryError // TODO: Simplify since error is always the same way, so dont have multiple constructors and so on
+    public class QueryError
     {
-        // /// <summary>
-        // /// Gets or sets the index
-        // /// </summary>
-        // public string Index { get; set; }
+        /// <summary>
+        /// Gets or sets the index
+        /// </summary>
         public string Index { get; set; }
+
+        /// <summary>
+        /// Gets the name used to identify the query.
+        /// </summary>
+        public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the error message.
         /// </summary>
         public string Message { get; set; }
 
-        // /// <summary>
-        // /// Gets or sets the errors
-        // /// </summary>
-        // public string Errors { get; set; }
+        /// <summary>
+        /// Gets or sets the errors
+        /// </summary>
         public string[] Errors { get; set; }
 
         public QueryError()
         {
         }
 
-        public QueryError(string index, string message, string[] errors = null)
+        public QueryError(string index, string name ,string message, string[] errors = null)
         {
             Index = index;
+            Name = name;
             Message = message;
             Errors = errors;
         }
